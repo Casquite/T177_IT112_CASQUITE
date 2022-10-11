c05275c007332af5bb0aa8a98176d821e2786007
Casquite <115531133+Casquite@users.noreply.github.com>
2022-10-11 17:32:27 +0800

    Add files via upload

---
+++ Hello_world.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+           //declaration of variables
+           int grade = 95;
+           char letter = 'b';
+           float num3 = 4.5;
+
+
+           //printing the variables
+           printf("value of num3 is %d\n", num3);
+           printf("value of letter is %c\n", letter);
+           printf("value of num3 is %f", num3);
+
+    return 0;
+}
